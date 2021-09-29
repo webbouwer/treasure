0d4359caa79948b0f0685f85b506546b8c212b4d
Webbouwer <support@webdesigndenhaag.net>
2021-09-29 08:56:25 +0200

    add responsive classes to body element for styling directions

--- views/parts/htmlhead.php
+++ views/parts/htmlhead.php
@@ -9,4 +9,31 @@ echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
 echo '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
 if ( ! isset( $content_width ) ) $content_width = 360;
 wp_head();
+?>
+<script>
+jQuery(function($) {
+  // on resize
+  var resizeThemeId;
+  $(window).resize(function() {
+    clearTimeout(resizeThemeId);
+    resizeThemeId = setTimeout(themeResizing, 20);
+  });
+
+  function themeResizing() {
+    var srcnw = $(window).width();
+    var cls = "medium";
+    if (srcnw < 460) {
+      cls = "small";
+    } else if (srcnw > 1100) {
+      cls = "large";
+    }
+    $('body').removeClass('medium small large');
+    $('body').addClass(cls);
+  }
+  $(window).ready(function() {
+    themeResizing();
+  });
+});
+</script>
+<?php
 echo '</head>';
