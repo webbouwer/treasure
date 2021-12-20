f3f9bdae75402b473cf7f8e8d1503f859c5ebe86
Oddsized <project@oddsized.org>
2021-12-20 16:46:39 +0100

    Artifact list item icons order css fix

--- views/parts/htmlhead.php
+++ views/parts/htmlhead.php
@@ -7,6 +7,9 @@ echo '<html '; language_attributes(); echo '><head>';
 echo '<meta http-equiv="Content-Type" content="text/html; charset='.get_bloginfo( 'charset' ).'" />';
 echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
 echo '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
+echo '<title>'.get_bloginfo( 'name' ).'</title>';
+echo '<meta name="description" content="De Hoekse Schatkist - Hoek van Holland Erfgoed en objecten - Cultuur en Archief ">';
+echo '<meta name="keywords" content="de hoekse, schatkist, de hoekse schatkist, chateau du lac, objecten, foto, video, gedicht, schilderij, verhaal, sculptuur, audio, pers, overige, erfgoed">';
 if ( ! isset( $content_width ) ) $content_width = 360;
 wp_head();
 ?>
